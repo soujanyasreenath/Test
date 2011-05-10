ec0b7a1a1a863d36cef1fdcdb1e0cb0bb62f204f
soujanya <soujanya@souj.(none)>
2011-05-10 12:35:14 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Souj::Application.config.secret_token = '97113f1a075656df6f2327d3f690e31a1d8bd698c7e66026bdd59e5ba5be4bac553a5cd6bd8efca94b7078595b108c1c1097bf31e60978c56a2f0551f95e7f07'
